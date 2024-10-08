3a6934666f8bd7efdd4ceb7f02d9cf7209d89205
DavidLagoUTN <98719124+DavidLagoUTN@users.noreply.github.com>
2024-10-08 11:42:36 -0300

    ADD: archivos primer parcial

--- RTA_SCRIPT_Examen_20241008/Punto_F.sh
+++ RTA_SCRIPT_Examen_20241008/Punto_F.sh
@@ -1,4 +1,4 @@
-echo "Mi IP Pública es: $(curl -s ifconfig.me)" > ~/repogit/UTNFRA_SO_1P2C_2024_Lago/RTA_ARCHIVOS_Examen_20241003/Filtro_Avanzado.txt
-echo "Mi usuario es $(whoami)" >> ~/repogit/UTNFRA_SO_1P2C_2024_Lago/RTA_ARCHIVOS_Examen_20241003/Filtro_Avanzado.txt
-sudo grep $USER /etc/shadow | awk -F ':' '{print "El Hash de mi Usuario es: " $2}' >> ~/repogit/UTNFRA_SO_1P2C_2024_Lago/RTA_ARCHIVOS_Examen_20241003/Filtro_Avanzado.txt
-git remote get-url origin | awk '{print "La URL de mi repositorio es: " $0}' >> ~/repogit/UTNFRA_SO_1P2C_2024_Lago/RTA_ARCHIVOS_Examen_20241003/Filtro_Avanzado.txt
+echo "Mi IP Pública es: $(curl -s ifconfig.me)" > ~/repogit/UTNFRA_SO_1P2C_2024_Lago/RTA_ARCHIVOS_Examen_20241008/Filtro_Avanzado.txt
+echo "Mi usuario es $(whoami)" >> ~/repogit/UTNFRA_SO_1P2C_2024_Lago/RTA_ARCHIVOS_Examen_20241008/Filtro_Avanzado.txt
+sudo grep $USER /etc/shadow | awk -F ':' '{print "El Hash de mi Usuario es: " $2}' >> ~/repogit/UTNFRA_SO_1P2C_2024_Lago/RTA_ARCHIVOS_Examen_20241008/Filtro_Avanzado.txt
+git remote get-url origin | awk '{print "La URL de mi repositorio es: " $0}' >> ~/repogit/UTNFRA_SO_1P2C_2024_Lago/RTA_ARCHIVOS_Examen_20241008/Filtro_Avanzado.txt
